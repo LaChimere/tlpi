b07fbf7435b178f30547f817d5a519068cbe862b
LaChimere <chingkun_lin@126.com>
2021-03-25 11:09:52 +0800

    add: syscall_speed.c

---
+++ progconc/syscall_speed.c
@@ -0,0 +1,26 @@
+#include "tlpi_hdr.h"
+
+#ifdef NOSYSCALL
+static int myfunc() { return 1; }
+#endif
+
+int main(int argc, char *argv[]) {
+    int numCalls, j;
+
+    numCalls = (argc > 1) ? getInt(argv[1], GN_GT_0, "num-calls") : 10000000;
+
+#ifdef NOSYSCALL
+    printf("Calling normal function\n");
+#else
+    printf("Calling getppid()\n");
+#endif
+
+    for (j = 0; j < numCalls; j++)
+#ifdef NOSYSCALL
+        myfunc();
+#else
+            getppid();
+#endif
+
+    exit(EXIT_SUCCESS);
+}
